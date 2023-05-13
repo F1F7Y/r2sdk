d5a7c77c83400103583bb4a354062f1812f2439a
F1F7Y <64418963+F1F7Y@users.noreply.github.com>
2023-05-13 20:40:09 +0200

    Initial push

---
+++ r2launcher/main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+int APIENTRY WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nShowCmd
+)
+{
+	return EXIT_SUCCESS;
+}
