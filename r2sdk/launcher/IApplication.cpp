e5403ba49124d1a121083d1dd7ae17253c8c2f36
F1F7Y <64418963+F1F7Y@users.noreply.github.com>
2023-05-25 16:47:59 +0200

    Add more math classes

--- r2sdk/launcher/IApplication.cpp
+++ r2sdk/launcher/IApplication.cpp
@@ -5,6 +5,7 @@
 #include "gameui/GameConsole.h"
 #include "filesystem.h"
 #include "engine/vgui_baseui_interface.h"
+#include "toolframework/itoolentity.h"
 
 int CModAppSystemGroup__Main(void* pModAppSystemGroup, void *a2)
 {
@@ -15,6 +16,7 @@ int CModAppSystemGroup__Main(void* pModAppSystemGroup, void *a2)
 	g_pGameConsole = Sys_GetFactoryPtr("client.dll", GAMECONSOLE_INTERFACE_VERSION).RCast<IGameConsole*>();
 	g_pFullFileSystem = Sys_GetFactoryPtr("filesystem_stdio.dll", FILESYSTEM_INTERFACE_VERSION).RCast<IFileSystem*>();
 	g_pMatSystemSurface = Sys_GetFactoryPtr("vguimatsurface.dll", MAT_SYSTEM_SURFACE_INTERFACE_VERSION).RCast<IMatSystemSurface*>();
+	g_pClientTools = Sys_GetFactoryPtr("client.dll", VCLIENTTOOLS_INTERFACE_VERSION).RCast<IClientTools*>();
 
 	return v_CModAppSystemGroup__Main(pModAppSystemGroup, a2);
 }
